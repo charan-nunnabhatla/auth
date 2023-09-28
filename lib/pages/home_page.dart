1069fe9b7f90ee20368aef4001c1e3b622fda253
Charan <charan.reings125@gmail.com>
2023-09-28 12:46:10 +0530

    now shows user email id

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,8 +1,6 @@
-import 'package:authlogin/pages/item_page.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:authlogin/utils/utils.dart';
 import 'package:logger/logger.dart';
 
 class HomePage extends StatefulWidget {
