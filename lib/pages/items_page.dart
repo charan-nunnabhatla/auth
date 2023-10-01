39dfccd7ddebfc2f829e3d6ec7339d6a0e185425
Charan <charan.reings125@gmail.com>
2023-10-01 17:26:46 +0530

    upload fixed

--- lib/pages/items_page.dart
+++ lib/pages/items_page.dart
@@ -1,4 +1,7 @@
+import 'package:authlogin/pages/home_page.dart';
 import 'package:authlogin/utils/utils.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 // import 'package:firebase_storage/firebase_storage.dart';
 
@@ -14,31 +17,42 @@ class _ItemsState extends State<Items> {
   final bodyController = TextEditingController();
   // final storage = FirebaseStorage.instanceFor(bucket: 'gs://lockify-8b531.appspot.com');
 
+  final FirebaseFirestore firebaseFirestore = FirebaseFirestore.instance;
+
+  void upload() {
+    final uid = FirebaseAuth.instance.currentUser!.uid;
+    firebaseFirestore
+        .collection('users')
+        .doc(uid)
+        .update({nameController.text.trim(): bodyController.text.trim()});
+    // Navigator.pop(context);
+    Navigator.pushReplacement(
+        context, MaterialPageRoute(builder: (context) => const HomePage()));
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Hero(
-      tag: 'floatingButton',
-      child: Scaffold(
-        body: SingleChildScrollView(
-          child: Column(
-            children: [
-              const SizedBox(
-                height: 200,
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            const SizedBox(
+              height: 200,
+            ),
+            const Text(
+              'Enter name and body that you want to store',
+              style: TextStyle(
+                fontFamily: 'arial',
+                fontSize: 15,
               ),
-              const Text(
-                'Enter name and body that you want to store',
-                style: TextStyle(
-                  fontFamily: 'arial',
-                  fontSize: 15,
-                ),
-              ),
-              const SizedBox(
-                height: 30,
-              ),
-              textFeild(nameController, 'Name', false),
-              textFeild(bodyController, 'Body', false, maxlines: null)
-            ],
-          ),
+            ),
+            const SizedBox(
+              height: 30,
+            ),
+            textFeild(nameController, 'Name', false),
+            textFeild(bodyController, 'Body', false, maxlines: null),
+            ElevatedButton(onPressed: upload, child: const Text('Save'))
+          ],
         ),
       ),
     );
